baae8ac715a47ed87064ea98547928940793e0d3
Mercy chepngeno <mercychepngeno582@gmail.com>
2025-07-05 13:26:35 +0300

    chore: setup TypeScript project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+async function fetchData() {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        console.log(response.data);
+    } catch (error) {
+        console.error('error fetching data', error); 
+    }
+}
+fetchData();
+//prints a message to the consolen
